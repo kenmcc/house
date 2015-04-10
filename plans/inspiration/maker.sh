2ca290ff747030fa86e8a9b21e19a5364bb860e9
Ken McCullagh <ken.mccullagh@s3group.com>
2015-04-10 10:47:49 +0100

    Auto commit of file plans/inspiration/maker.sh

--- plans/inspiration/maker.sh
+++ plans/inspiration/maker.sh
@@ -17,6 +17,25 @@ if [ "$statResult" = "" ]; then
 fi
 
 echo "Doing stuff"
+# we should check in the untracked files
 
+untracked=`git status --porcelain | grep "^??" | sed -e 's/^?? //'`
+modified=`git status --porcelain | grep "^ M" | sed -e 's/^ M//'`
+deleted=`git status --porcelain | grep "^D " | sed -e 's/^D //'`
+echo "UNTRACKED: ADD"
+for l in $untracked ; do
+    echo $l
+    git add "$l"
+    git commit -m "added file $l" ../../$l
+done
 
+echo "MODIFIED, COMMIT"
+for l in $modified ; do
+    git commit -m "Auto commit of file $l" ../../$l
+done
+
+echo "DELETED, REMOVE"
+for l in $deleted ; do
+    git commit -m "DELETE FILE $l", ../../$l
+done
 
